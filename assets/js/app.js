ad2e410d2f88034534b4df6e6ab31b1f1ee2f454
damir <damir1petkovic@gmail.com>
2023-08-08 12:05:15 +0200

    install alpine focus

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,6 +22,7 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 import Alpine from 'alpinejs'
+import focus from '@alpinejs/focus'
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
@@ -29,6 +30,7 @@ window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
 window.Alpine = Alpine;
+Alpine.plugin( focus);
 Alpine.start();
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
